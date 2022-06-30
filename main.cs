8aaeaa735e899a2840dd553896f26dc9795f65b6
salvoch <github@salvoch.com>
2022-06-30 00:56:12 -0600

    Add project files.

---
+++ main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Config;
+
+namespace MainSpace
+{
+
+    class RunProgram
+    {
+        public static void Main(string[] args)
+        {
+            ConfigReader cfg = new ConfigReader();
+            cfg.get_config();
+            cfg.print_cfg();
+
+        }
+    }
+
+}
---
+++ src/utils/config.cs
@@ -0,0 +1,57 @@
+﻿/*
+Reads the config.yml file and returns it as a struct
+The config.yml file contains the personalized configuration
+parameters needed for our program to work
+*/
+using System;
+using System.IO;
+
+
+namespace Config
+{
+    class ConfigReader
+    {
+        /// ConfigFile structure, these are the fields in our config.yml file
+        public string? username;
+        public string? userID;
+        public string? eep_path;
+        public string? eep_file;
+        public string? db_endpoint;
+
+        public void get_config()
+        {
+            //Read config file and parse out into each member variable
+            string[] fileText = File.ReadAllLines("config.txt");
+
+            username = parse_yaml_line(fileText[0]);
+            userID = parse_yaml_line(fileText[1]);
+            eep_path = parse_yaml_line(fileText[2]);
+            eep_file = parse_yaml_line(fileText[3]);
+            db_endpoint = parse_yaml_line(fileText[4]);
+
+        }
+        public void print_cfg()
+        {
+            //Test method to print out our member values
+            Console.WriteLine("Username: " + username);
+            Console.WriteLine("userID: " + userID);
+            Console.WriteLine("eep_path: " + eep_path);
+            Console.WriteLine("eep_file: " + eep_file);
+            Console.WriteLine("db_endpoint: " + db_endpoint);
+        }
+
+        private string parse_yaml_line(string fileline)
+        {
+            //provide a list of delimeters
+            //Split the string, and return only what's on the right hand side of any ":"
+            string[] delimeters = { " : ", ": ", ":"};
+            string[] words = fileline.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
+
+            //Drop the first element, combine the rest of the array elements
+            //We have to do this since one of the text fields is a url, which has https://...
+            words = words.Skip(1).ToArray();
+            return string.Join("",words);
+        }
+    }
+
+}
