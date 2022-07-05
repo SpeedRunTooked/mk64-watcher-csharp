74bf58b8069a664224852f27855230aa58cb631e
salvoch <github@salvoch.com>
2022-07-05 00:22:45 -0600

    Next steps notes

--- main.cs
+++ main.cs
@@ -22,7 +22,24 @@ namespace MainSpace
             //Test for all_records data structure
             watcher.display_all_records();
 
-            //NEXT STEPS, make sure we can compare two wathers, do we just do ==? or do we write a function for this
+            /*NEXT STEPS, THIS DOESN'T WORK!!!
+             * 2 options
+             * -- Write a method which takes 2 "watcher" objects, and iterates through them
+             * -- Remake watcher, to return an array which has {name, record[6] = {character, time}}
+             * ---- Or remove char to simplify?
+             * ---- ??
+             */
+
+            EepWatcher watcher2 = new EepWatcher();
+            watcher2.read_directly();
+
+            if (watcher.all_records == watcher2.all_records)
+            {
+                Console.WriteLine("They Match!");
+            } else
+            {
+                Console.WriteLine("They Don't");
+            }
 
         }
     }
